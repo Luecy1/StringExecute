eb12120e3b902b37bc3f57ca135c843d7d0fd40b
Youhei Oonuma <oonuma617@gmail.com>
2019-12-22 20:14:40 +0900

    add test

--- src/test/java/StringExtTest.kt
+++ src/test/java/StringExtTest.kt
@@ -1,14 +1,35 @@
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.io.File
 
 class StringExtTest {
 
     @Test
     fun StringExecuteTest() {
-
         // only mac or linux
-        val expect = "hello world\n"
-        val actual = "echo hello world".execute()
+        val expect = "200"
+        val actual = "curl -LI http://example.com -o /dev/null -w %{http_code} -s".execute()
+
+        assertEquals(expect, actual)
+    }
+
+    @Test
+    fun StringExecuteTest2() {
+        // only mac or linux
+        val expect = System.getProperty("user.home")
+        val currentDirectory = File(expect)
+        val actual = "pwd -L".execute(currentDirectory)
+
+        assertEquals(expect + "\n", actual)
+    }
+
+    @Test
+    fun StringExecuteTest3() {
+        // only mac or linux
+        val expect = "hogehoge\n"
+        val environmentMap = mapOf("TEST" to "hogehoge")
+        val actual = "./src/test/resources/EchoVariant.sh \$PATH"
+            .execute(environmentMap = environmentMap)
 
         assertEquals(expect, actual)
     }
